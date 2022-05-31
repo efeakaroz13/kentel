b7c44cbf7017a64c5eaa640092ea3986c813984a
Efe Akaröz <69296379+KentelDevelopment@users.noreply.github.com>
2022-05-31 21:41:16 +0300

    register page

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+#Copyright (c) 2022 Efe Akaröz
+
 from flask import Flask,render_template,request,redirect
 from auth import auth
 from userEditor import userEditor
 
-print(userEditor.getUserData("efeakaroz13"))
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register",methods = ["POST","GET"])
+def register():
+    return render_template("register.html")
+
+app.run(debug=True)
