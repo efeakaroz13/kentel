fb5ac1ee66053ea54bc83afaba3be12b11a1da62
Efe Akaröz <69296379+KentelDevelopment@users.noreply.github.com>
2022-06-05 09:36:19 +0300

    Design upgrades for sending messages

---
+++ answer.py
@@ -0,0 +1,8 @@
+while True:
+    go_back = input("Will you like to try another menu option?: ")
+    if go_back == "Yes":
+        print("yes")
+        continue
+    else:
+        print("We'll continue with your current choice")
+    break
