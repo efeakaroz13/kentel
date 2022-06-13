878e45ea076cef7cba9986bfd57cc1e970605a3c
Efe Akaröz <69296379+KentelDevelopment@users.noreply.github.com>
2022-06-13 14:59:51 +0300

    scroll down js update

--- static/loadmsg.js
+++ static/loadmsg.js
@@ -1,29 +1,38 @@
+
 function loadmsg(){
     var username;
     
-    $.getJSON("/getUsername",function(data){
-        username = data.username
-        $.getJSON("/fetch/msgs",function(data){
-            console.log(data.val.length)
-            document.getElementById("msgs").innerHTML = "";
-            data.val.reverse();
-            for (let c = 0; c < data.val.length; c++) {
-                const element = data.val[c];
-                console.log(element)
-                if (data["val"][c]["sender"] == username) {
-                    userclass = "sender";
-                    
-                }
-                else{
-                    userclass = "reciever";
-                    
-                }
-                
-                document.getElementById("msgs").innerHTML = document.getElementById("msgs").innerHTML+"<p class="+userclass+">"+data["val"][c]["content"]+"</p>";
-                document.scrollingElement.scrollBy(0,1000)
+    $.getJSON("/getUsername",function(data1){
 
-                
+        oldData = window.localStorage.getItem("old")
+        console.log(oldData);
+        username = data1.username
+        $.getJSON("/fetch/msgs",function(data){
+
+            if (data["val"].length != oldData){
+                // console.log(data.val.length)
+                document.getElementById("msgs").innerHTML = "";
+                data.val.reverse();
+                for (let c = 0; c < data.val.length; c++) {
+                    const element = data.val[c];
+
+                    if (data["val"][c]["sender"] == username) {
+                        userclass = "sender";
+                        
+                    }
+                    else{
+                        userclass = "reciever";
+                        
+                    }
+
+                    
+                    document.getElementById("msgs").innerHTML = document.getElementById("msgs").innerHTML+"<p class="+userclass+">"+data["val"][c]["content"]+"</p>";
+                    document.scrollingElement.scrollBy(0,1000)
+
+                    
+                }
             }
+            window.localStorage.setItem('old', data["val"].length); 
         })
     })
 
